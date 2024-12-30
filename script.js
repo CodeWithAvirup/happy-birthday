cc78466cd30eee81d294b1c3cdfa2a7770505f07
Avirup Chatterjee <156928846+CodeWithAvirup@users.noreply.github.com>
2024-12-30 14:38:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// import { Spheres1Background } from 'https://cdn.jsdelivr.net/npm/threejs-components@0.0.5/build/backgrounds/spheres1.cdn.min.js'
+document.addEventListener('DOMContentLoaded', () => {
+    const { Spheres1Background } = window;
+    const bg = Spheres1Background(document.getElementById('webgl-canvas'), {
+        count: 300,
+        minSize: 0.3,
+        maxSize: 1,
+        gravity: 0.5,
+    });
+
+    console.log(Spheres1Background);
+
+    document.body.addEventListener('click', () => {
+        bg.spheres.setColors([
+            0xffffff * Math.random(),
+            0xffffff * Math.random(),
+            0xffffff * Math.random(),
+        ]);
+    });
+
+    document.body.addEventListener('keydown', () => {
+        bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0;
+    });
+
+    document.getElementById('gravity-btn').addEventListener('click', () => {
+        bg.spheres.config.gravity = bg.spheres.config.gravity === 0 ? 1 : 0;
+    });
+
+    document.getElementById('colors-btn').addEventListener('click', () => {
+        bg.spheres.setColors([
+            0xffffff * Math.random(),
+            0xffffff * Math.random(),
+            0xffffff * Math.random(),
+        ]);
+    });
+});
